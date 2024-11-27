52e247075055bc58b409beec62c806c7c5afee39
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-11-27 17:05:54 +0100

    Created and implemented

---
+++ src/components/Meeting/MeetingList.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { CiEdit, CiTrash } from "react-icons/ci";
+
+const MeetingList = ({ meetings }) => {
+  return (
+    <div className="col-md-12">
+      <div
+        className="border p-3"
+        style={{
+          borderRadius: "5px",
+          backgroundColor: "white",
+        }}
+      >
+        <h2>List of Created Meetings</h2>
+        <table className="table table-white">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Meeting Title</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Level</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {meetings.map((meeting, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{meeting.title}</td>
+                <td>{meeting.date}</td>
+                <td>{meeting.time}</td>
+                <td>{meeting.level}</td>
+                <td>
+                  <button className="btn btn-warning btn-sm me-2">
+                    <CiEdit />
+                  </button>
+                  <button className="btn btn-danger btn-sm">
+                    <CiTrash />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default MeetingList;
---
+++ src/components/Meeting/meetingForm.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const MeetingForm = ({ addMeeting }) => {
+  const [formData, setFormData] = useState({
+    title: "",
+    date: "",
+    time: "",
+    level: "",
+    participants: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addMeeting(formData);
+
+    setFormData({
+      title: "",
+      date: "",
+      time: "",
+      level: "",
+      participants: "",
+      description,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label className="form-label">Meeting Title</label>
+        <input
+          placeholder="Enter meeting title"
+          type="text"
+          className="form-control"
+          onChange={handleChange}
+          name="title"
+          value={formData.title}
+          required
+        />
+      </div>
+      <div className="row">
+        <div className="mb-3 col-md-6">
+          <label className="form-label">Meeting Date</label>
+          <input
+            type="date"
+            className="form-control"
+            onChange={handleChange}
+            name="date"
+            value={formData.date}
+            required
+          />
+        </div>
+        <div className="mb-3 col-md-6">
+          <label className="form-label">Meeting Time</label>
+          <input
+            type="time"
+            className="form-control"
+            onChange={handleChange}
+            name="time"
+            value={formData.time}
+            required
+          />
+        </div>
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Meeting Level</label>
+        <select
+          className="form-control"
+          onChange={handleChange}
+          name="level"
+          value={formData.level}
+          required
+        >
+          <option value="">Choose Level</option>
+          <option value={"team"}>Team</option>
+          <option value={"department"}>Department</option>
+          <option value={"company"}>Company</option>
+        </select>
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Participants</label>
+        <input
+          placeholder="Enter participant emails"
+          type="text"
+          className="form-control"
+          onChange={handleChange}
+          name="participants"
+          value={formData.participants}
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Description</label>
+        <textarea
+          placeholder="Enter meeting description"
+          type="text"
+          className="form-control"
+          onChange={handleChange}
+          name="description"
+          value={formData.description}
+          required
+          rows="3"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Create Meeting
+      </button>
+    </form>
+  );
+};
+
+export default MeetingForm;
