ec8b6220505c214f868125bf04d625941c0a068a
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-22 19:41:48 +0100

    Delete Meeting-API directory

--- Meeting-API/src/main/java/se/lexicon/meetingapi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package se.lexicon.meetingapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.lexicon.meetingapi.entity.User;
-
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
